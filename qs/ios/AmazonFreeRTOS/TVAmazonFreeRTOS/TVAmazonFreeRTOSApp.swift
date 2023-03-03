312c83d733bc05f2fb1b60bb234d7ba61ea9cfb5
Tuyen Vu <hi@tuyen-vuduc.tech>
2023-03-03 23:04:39 +0700

    - Initial AmazonFreeRTOS.iOS

---
+++ qs/ios/AmazonFreeRTOS/TVAmazonFreeRTOS/TVAmazonFreeRTOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVAmazonFreeRTOSApp.swift
+//  TVAmazonFreeRTOS
+//
+//  Created by Tuyen Vu on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TVAmazonFreeRTOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
