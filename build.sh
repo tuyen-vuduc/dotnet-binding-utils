645f206e091abe06e65a71ac9b18d89f3c23ce7f
Tuyen Vu <hi@tuyen-vuduc.tech>
2023-08-26 12:09:08 +0700

    - note build QS helper sln

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 dotnet nuget locals -c all
 dotnet pack -c Release -o nugets src/Dependencies.Gradle/Dependencies.Gradle.csproj
 
-dotnet build -c Release dependencies.gradle.qs.sln
+# dotnet build -c Release dependencies.gradle.qs.sln
