360499d77930ef4a6f3e8e2fca513aa7a3a6dce8
Tuyen Vu <hi@tuyen-vuduc.tech>
2023-08-27 00:42:41 +0700

    - metadata: Xamarin.AndroidX.Compose.UI.Util

--- src/MetadataFetcher.Tests/FetcherTests.cs
+++ src/MetadataFetcher.Tests/FetcherTests.cs
@@ -11,6 +11,9 @@ public class FetcherTests
     }
 
     [Theory]
+    [InlineData("Xamarin.AndroidX.Compose.UI.Util", "androidx.compose.ui", "ui-util")]
+    [InlineData("Xamarin.AndroidX.Compose.UI.Unit", "androidx.compose.ui", "ui-unit")]
+    [InlineData("Xamarin.AndroidX.Compose.UI.Graphics", "androidx.compose.ui", "ui-graphics")]
     [InlineData("Xamarin.GooglePlayServices.InstantApps", "com.google.android.instantapps", "instantapps")]
     [InlineData("Xamarin.Google.Accompanist.FlowLayout", "com.google.accompanist", "accompanist-flowlayout")]
     [InlineData("Xamarin.Google.Accompanist.SystemUIController", "com.google.accompanist", "accompanist-systemuicontroller")]
