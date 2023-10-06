d9371b7237918ff711a25e5e78f672d118056cf5
Tuyen Vu <hi@tuyen-vuduc.tech>
2023-10-07 06:02:55 +0700

    - add binding adjustments for google accompanist

---
+++ qs/com.stripe--stripe-android/source/com.google.accompanist/accompanist-navigation-animation/Additions/Additions.cs
@@ -0,0 +1,9 @@
+using Java.Lang;
+
+namespace Com.Google.Accompanist.Navigation.Animation
+{
+    partial class AnimatedComposeNavigator
+    {
+        public override Object CreateDestination() => CreateDestinationX();
+    }
+}
---
+++ qs/com.stripe--stripe-android/source/com.google.accompanist/accompanist-navigation-material/Additions/Additions.cs
@@ -0,0 +1,9 @@
+using Java.Lang;
+
+namespace Com.Google.Accompanist.Navigation.Material 
+{
+    partial class BottomSheetNavigator
+    {
+        public override Object CreateDestination() => CreateDestinationX();
+    }
+}
