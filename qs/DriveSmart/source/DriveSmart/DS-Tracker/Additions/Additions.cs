9a98a3dfa5f02e99c6d3ef1c14ffc2f5a809ee55
Tuyen Vu <hi@tuyen-vuduc.tech>
2024-03-19 07:20:29 +0700

    - add support methods

--- qs/DriveSmart/source/DriveSmart/DS-Tracker/Additions/Additions.cs
+++ qs/DriveSmart/source/DriveSmart/DS-Tracker/Additions/Additions.cs
@@ -12,3 +12,55 @@ namespace Com.Dstracker.Utils
 namespace Com.Dstracker {
     partial class EventEntry : global::Android.Provider.IBaseColumns { }
 }
+
+namespace Com.Dstracker.Singleton
+{
+    using System;
+    using Com.Dstracker.Enums;
+    using Kotlin.Jvm.Functions;
+
+    partial class Tracker
+    {
+        public void GetOrAddUserIdBy(string userId, Action<Outcome> action)
+        {
+            var func1 = new Function1Action<Outcome>(action);
+            this.GetOrAddUserIdBy(userId, func1);
+        }
+        public void Configure(string userId, Action<Outcome> action)
+        {
+            var func1 = new Function1Action<Outcome>(action);
+            this.SetUserId(userId, func1);
+        }
+        public void SetUserId(string userId, Action<Outcome> action)
+        {
+            var func1 = new Function1Action<Outcome>(action);
+            this.SetUserId(userId, func1);
+        }
+        public void Start(string withMetaData, Action<Outcome> action)
+        {
+            var func1 = new Function1Action<Outcome>(action);
+            this.Start(withMetaData, func1);
+        }
+        public void Stop(string userId, Action<Outcome> action)
+        {
+            var func1 = new Function1Action<Outcome>(action);
+            this.Stop(func1);
+        }
+    }
+
+    public class Function1Action<T> : Java.Lang.Object, IFunction1
+    {
+        private readonly Action<T> action;
+
+        public Function1Action(Action<T> action)
+        {
+            this.action = action;
+        }
+
+        public Java.Lang.Object? Invoke(Java.Lang.Object? p0)
+        {
+            action?.Invoke((T)(object)p0);
+            return null;
+        }
+    }
+}
