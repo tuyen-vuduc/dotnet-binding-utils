0be1e6127e8112db07822016a97448d64764d63d
Tuyen Vu <hi@tuyen-vuduc.tech>
2024-03-21 06:16:46 +0700

    - add homge page url and guide url to group.json

--- src/libs/Binderator.Gradle/Model/GroupModel.cs
+++ src/libs/Binderator.Gradle/Model/GroupModel.cs
@@ -7,6 +7,8 @@ public record GroupModel
     [JsonIgnore]
     public string Id { get; set; }
     public string Name { get; set; }
+    public string HomePageUrl { get; set; }
+    public string GuideUrl { get; set; }
     [JsonIgnore]
     public string Icon { get; set; }
     [JsonIgnore]
