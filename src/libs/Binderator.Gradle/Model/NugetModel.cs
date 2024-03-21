c10ab5b390db2bbf7fee36cd8b8e51446bf0067f
Tuyen Vu <tuyenvd@newwave.com.vn>
2024-03-21 14:21:33 +0700

    - default DependencyOnly to false - facebook: infer, soloader and bolts-tasks are ok

--- src/libs/Binderator.Gradle/Model/NugetModel.cs
+++ src/libs/Binderator.Gradle/Model/NugetModel.cs
@@ -11,7 +11,7 @@ public record NuGetModel
     public string HomePageUrl { get; set; }
     public string GuideUrl { get; set; }
     public string Relocated { get; set; }
-    public bool DependencyOnly { get; set; } = true;    
+    public bool DependencyOnly { get; set; }
     [JsonIgnore]
     public string Icon { get; set; }
     public IEnumerable<VersionMappingModel> VersionMappings { get; set; } = new VersionMappingModel[0];
