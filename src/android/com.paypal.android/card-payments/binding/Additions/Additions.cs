cc43c4c8dd3db362d27f76443cfa7b5dccc9c438
Tuyen VU <hi@tuyen-vuduc.tech>
2024-04-02 17:32:59 +0700

    feat: com.paypal.android:card-payments:1.3.0 (#33)
    * - update metadata
    
    * - com.paypal.android:card-payments:1.3.0

---
+++ src/android/com.paypal.android/card-payments/binding/Additions/Additions.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using Android.Runtime;
+using Java.Interop;
+
+namespace Com.Paypal.Android.Cardpayments
+{
+    partial class Card
+    {
+        partial class Creator
+        {
+            global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel parcel)
+            {
+                return this.CreateFromParcel(parcel);
+            }
+            global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size)
+            {
+                return this.NewArray(size);
+            }
+        }
+    }
+    partial class CardVaultRequest
+    {
+        partial class Creator
+        {
+            global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel parcel)
+            {
+                return this.CreateFromParcel(parcel);
+            }
+            global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size)
+            {
+                return this.NewArray(size);
+            }
+        }
+    }
+    partial class CardRequest
+    {
+        partial class Creator
+        {
+            global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel parcel)
+            {
+                return this.CreateFromParcel(parcel);
+            }
+            global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size)
+            {
+                return this.NewArray(size);
+            }
+        }
+    }
+    partial class CardVaultResult
+    {
+        partial class Creator
+        {
+            global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel parcel)
+            {
+                return this.CreateFromParcel(parcel);
+            }
+            global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size)
+            {
+                return this.NewArray(size);
+            }
+        }
+    }
+}
---
+++ src/android/com.paypal.android/core-payments/binding/Additions/Additions.cs
@@ -0,0 +1,17 @@
+namespace Com.Paypal.Android.Corepayments
+{
+    partial class Address
+    {
+        partial class Creator
+        {
+            global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel parcel)
+            {
+                return this.CreateFromParcel(parcel);
+            }
+            global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size)
+            {
+                return this.NewArray(size);
+            }
+        }
+    }
+}
