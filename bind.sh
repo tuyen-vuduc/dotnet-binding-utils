c3e6d6de936a94219c41f50bff28f6a2026b9ccf
Tuyen VU <hi@tuyen-vuduc.tech>
2024-04-04 16:39:54 +0700

    - mapbox android v11.2.2 (#34)

--- bind.sh
+++ bind.sh
@@ -2,4 +2,5 @@
 dotnet tool restore
 dotnet cake "$@"
 dotnet clean ./src/libs/BindingHost/BindingHost.csproj
+dotnet restore ./src/libs/BindingHost/BindingHost.csproj
 dotnet run --project ./src/libs/BindingHost/BindingHost.csproj -- --base-path=$PWD "$@"
