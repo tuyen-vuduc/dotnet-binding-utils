3cec13009ded86ca99ef6b837023d688d11d4680
Tuyen Vu <hi@tuyen-vuduc.tech>
2024-04-09 07:46:42 +0700

    - com.google.dagger:dagger:2.50

---
+++ src/android/com.google.dagger/dagger/binding/Additions/Additions.cs
@@ -0,0 +1,17 @@
+
+namespace Dagger.Internal 
+{
+    partial class ProviderOfLazy
+    {
+        global::Java.Lang.Object global::JavaX.Inject.IProvider.Get() => (global::Java.Lang.Object )(object)Get();
+    }
+    partial class SetFactory
+    {
+        global::Java.Lang.Object global::JavaX.Inject.IProvider.Get() => (global::Java.Lang.Object )(object)Get();
+    }
+
+    partial class MapProviderFactory : global::Dagger.ILazy
+    {
+        global::Java.Lang.Object global::Dagger.ILazy.Get() => (global::Java.Lang.Object )(object)Get();
+    }
+}
