6cbb425f63a31c0a4c050f1ded65163dabea59b9
Tuyen Vu <hi@tuyen-vuduc.tech>
2024-04-11 06:26:47 +0700

    - WIP org.bouncycastle:bcprov-jdk15to18:1.69

---
+++ src/android/org.bouncycastle/bcprov-jdk15to18/binding/Additions/Additions.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using Android.Runtime;
+using Java.Interop;
+
+namespace Org.Bouncycastle.Crypto
+{
+//    partial class StreamBlockCipher : global::Org.Bouncycastle.Crypto.IBlockCipher
+//    {
+//        static Delegate? cb_getBlockSize;
+//#pragma warning disable 0169
+//        static Delegate GetGetBlockSizeHandler()
+//        {
+//            if (cb_getBlockSize == null)
+//                cb_getBlockSize = JNINativeWrapper.CreateDelegate((_JniMarshal_PP_I)n_GetBlockSize);
+//            return cb_getBlockSize;
+//        }
+
+//        static int n_GetBlockSize(IntPtr jnienv, IntPtr native__this)
+//        {
+//            var __this = global::Java.Lang.Object.GetObject<global::Org.Bouncycastle.Crypto.StreamBlockCipher>(jnienv, native__this, JniHandleOwnership.DoNotTransfer)!;
+//            return __this.BlockSize;
+//        }
+//#pragma warning restore 0169
+
+//        public abstract int BlockSize
+//        {
+//            // Metadata.xml XPath method reference: path="/api/package[@name='org.bouncycastle.crypto']/class[@name='StreamBlockCipher']/method[@name='getBlockSize' and count(parameter)=0]"
+//            [Register("getBlockSize", "()I", "GetGetBlockSizeHandler")]
+//            get;
+//        }
+//    }
+}
