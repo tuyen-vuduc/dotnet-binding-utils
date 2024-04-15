d671933bba75159bff95a95c29b340c625d39bb4
Tuyen Vu <hi@tuyen-vuduc.tech>
2024-04-16 05:48:04 +0700

    - payment-model

---
+++ src/android/com.stripe/payments-model/binding/Additions/Additions.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using Android.Runtime;
+using Java.Interop;
+
+namespace Com.Stripe.Android.Cards
+{
+    partial class Bin
+    {
+        partial class Creator
+        {
+            unsafe global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel parcel)
+                => CreateFromParcel(parcel);
+
+            unsafe global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size)
+                => NewArray(size).Cast<global::Java.Lang.Object>().ToArray();
+        }
+    }
+}
+
+namespace Com.Stripe.Android.Model
+{
+    partial class ConsumerSessionLookup
+    {
+        partial class Creator
+        {
+            unsafe global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel parcel)
+                => CreateFromParcel(parcel);
+
+            unsafe global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size)
+                => NewArray(size).Cast<global::Java.Lang.Object>().ToArray();
+        }
+    }
+    partial class Card
+    {
+        partial class Creator
+        {
+            unsafe global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel parcel)
+                => CreateFromParcel(parcel);
+
+            unsafe global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size)
+                => NewArray(size).Cast<global::Java.Lang.Object>().ToArray();
+        }
+    }
+    partial class BankAccount
+    {
+        partial class Creator
+        {
+            unsafe global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel parcel)
+                => CreateFromParcel(parcel);
+
+            unsafe global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size)
+                => NewArray(size).Cast<global::Java.Lang.Object>().ToArray();
+        }
+    }
+    partial class Token
+    {
+        partial class Creator
+        {
+            unsafe global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel parcel)
+                => CreateFromParcel(parcel);
+
+            unsafe global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size)
+                => NewArray(size).Cast<global::Java.Lang.Object>().ToArray();
+        }
+    }
+    partial class ConsumerSession
+    {
+        partial class Creator
+        {
+            unsafe global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel parcel)
+                => CreateFromParcel(parcel);
+
+            unsafe global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size)
+                => NewArray(size).Cast<global::Java.Lang.Object>().ToArray();
+        }
+
+        partial class VerificationSession
+        {
+            partial class SessionState
+            {
+                partial class Creator
+                {
+                    unsafe global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel parcel)
+                        => CreateFromParcel(parcel);
+
+                    unsafe global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size)
+                        => NewArray(size).Cast<global::Java.Lang.Object>().ToArray();
+                }
+            }
+            partial class SessionType
+            {
+                partial class Creator
+                {
+                    unsafe global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel parcel)
+                        => CreateFromParcel(parcel);
+
+                    unsafe global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size)
+                        => NewArray(size).Cast<global::Java.Lang.Object>().ToArray();
+                }
+            }
+            partial class Creator
+            {
+                unsafe global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel parcel)
+                    => CreateFromParcel(parcel);
+
+                unsafe global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size)
+                    => NewArray(size).Cast<global::Java.Lang.Object>().ToArray();
+            }
+        }
+    }
+}
