63a2d8caee494fec9160941a2d67a25daf89e8cd
Tuyen Vu <hi@tuyen-vuduc.tech>
2024-05-07 04:38:09 +0700

    - remove qs shakebugs

--- qs/com.shakebugs/source/com.shakebugs/shake/Additions/Additions.cs
+++
@@ -1,9 +0,0 @@
-using Java.Lang;
-
-namespace Com.Shakebugs.Shake.Internal.Domain.Models
-{
-    partial class ActivityHistoryEvent : global::Java.Lang.IComparable
-    {
-        public int CompareTo(Object o) => CompareTo(o as ActivityHistoryEvent);
-    }
-}
