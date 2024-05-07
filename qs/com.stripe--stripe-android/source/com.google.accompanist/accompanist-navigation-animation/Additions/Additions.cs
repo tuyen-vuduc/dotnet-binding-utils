bf2b423100e63c174fef485c701f20e23fc4b81b
Tuyen VU <hi@tuyen-vuduc.tech>
2024-05-07 07:13:57 +0700

    feat: Braintree Drop-in v6.16.0 (#39)
    * - update metadata
    
    * - update metadata
    
    * - update metadata
    
    * - com.braintreepayments.api:drop-in:6.16.0
    
    * - com.braintreepayments.api:drop-in:6.16.0

--- qs/com.stripe--stripe-android/source/com.google.accompanist/accompanist-navigation-animation/Additions/Additions.cs
+++
@@ -1,9 +0,0 @@
-using Java.Lang;
-
-namespace Com.Google.Accompanist.Navigation.Animation
-{
-    partial class AnimatedComposeNavigator
-    {
-        public override Object CreateDestination() => CreateDestinationX();
-    }
-}
--- qs/com.stripe--stripe-android/source/com.google.accompanist/accompanist-navigation-material/Additions/Additions.cs
+++
@@ -1,9 +0,0 @@
-using Java.Lang;
-
-namespace Com.Google.Accompanist.Navigation.Material 
-{
-    partial class BottomSheetNavigator
-    {
-        public override Object CreateDestination() => CreateDestinationX();
-    }
-}
