8015f096405214dbcc26e793718bc5ddc5eb220c
Tuyen Vu <hi@tuyen-vuduc.tech>
2024-05-08 00:23:07 +0700

    - attempt 3

--- bind.sh
+++ bind.sh
@@ -1,18 +1,5 @@
 
 rm -rf ./src/libs/BindingHost/*.props
-if [[ $3 == "--dotnet" ]] && [[ $4 == 8.* ]] ;
-then
-    echo "DOTNET8"
-    cp global.8.json global.json
-else
-    echo "DOTNET7 and below"
-    cp global.7.json global.json
-fi
-
-echo "Install Android workload"
-dotnet workload install android
-
-echo "Main steps"
 dotnet tool restore
 dotnet cake "$@"
 dotnet clean ./src/libs/BindingHost/BindingHost.csproj
