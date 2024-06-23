0398b9f3cdb719c6fd948cbaf85c4882122d9cd5
Tuyen Vu <tuyen.vuduc0902@gmail.com>
2024-06-23 12:42:23 +0700

    - restore original script

--- src/ios/symbol.sh
+++ src/ios/symbol.sh
@@ -3,5 +3,5 @@ cls=$2
 
 echo "$framework $cls"
 
-nm -m "/Volumes/ws/tv/dotnet-binding-utils/src/ios/Stripe/adapters/StripePaymentsheetObjC/Carthage/Build/StripePayments.xcframework/ios-arm64/StripePayments.framework/StripePayments" \
- | grep "_OBJC_CLASS_\$_TtC14StripePayments35STPIntentActionAlipayHandleRedirect"
+nm -m "$framework/artifacts/$framework.xcframework/ios-arm64/$framework.framework/$framework" \
+ | grep "_OBJC_CLASS_\$_$cls"
