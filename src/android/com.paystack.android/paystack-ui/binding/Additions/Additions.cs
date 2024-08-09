2d5f42c5a016d55ad850436025d2886c3a99eb85
Tuyen Vu <tuyenvd@newwave.com.vn>
2024-08-09 11:27:35 +0700

    - com.paystack.android:paystack-ui:0.0.9

---
+++ src/android/com.paystack.android/paystack-ui/binding/Additions/Additions.cs
@@ -0,0 +1,53 @@
+#nullable restore
+using System;
+using System.Collections.Generic;
+using Android.Runtime;
+using Java.Interop;
+
+namespace Com.Paystack.Android.UI.Models
+{
+    partial class PaymentCompletionDetails
+    {
+        partial class Creator
+        {
+            global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel? source) => this.CreateFromParcel(source);
+            global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size) => this.NewArray(size);
+        }
+    }
+}
+namespace Com.Paystack.Android.UI.Paymentsheet
+{
+    partial class PaymentSheetResult
+    {
+        partial class Cancelled
+        {
+            partial class Creator
+            {
+                global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel? source) => this.CreateFromParcel(source);
+                global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size) => this.NewArray(size);
+            }
+        }
+    }
+    partial class PaymentSheetResult
+    {
+        partial class Completed
+        {
+            partial class Creator
+            {
+                global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel? source) => this.CreateFromParcel(source);
+                global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size) => this.NewArray(size);
+            }
+        }
+    }
+    partial class PaymentSheetResult
+    {
+        partial class Failed
+        {
+            partial class Creator
+            {
+                global::Java.Lang.Object global::Android.OS.IParcelableCreator.CreateFromParcel(global::Android.OS.Parcel? source) => this.CreateFromParcel(source);
+                global::Java.Lang.Object[] global::Android.OS.IParcelableCreator.NewArray(int size) => this.NewArray(size);
+            }
+        }
+    }
+}
