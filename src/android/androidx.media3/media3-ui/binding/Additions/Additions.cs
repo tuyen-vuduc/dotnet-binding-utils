073b26e97a32b4842c552320bfc986319d8c93a0
Tuyen VU <hi@tuyen-vuduc.tech>
2024-08-13 12:21:50 +0700

    feat: androidx.media3 media3-ui 1.4.0 (#79)
    * - androidx.media3:media3-ui:1.4.0
    
    * .

---
+++ src/android/androidx.media3/media3-ui/binding/Additions/Additions.cs
@@ -0,0 +1,12 @@
+#nullable restore
+using System;
+using System.Collections.Generic;
+using Android.Runtime;
+using Java.Interop;
+
+namespace Androidx.Media3.UI
+{
+    partial class DefaultTimeBar {
+        void global::Androidx.Media3.UI.ITimeBar.SetEnabled(bool value) => Enabled = value;
+    }
+}
