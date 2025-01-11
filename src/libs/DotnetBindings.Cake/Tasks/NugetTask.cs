261cee8a6df75724b1640a0ad22b354588669dbf
Tuyen Vu <tuyen.vuduc0902@gmail.com>
2025-01-11 09:57:51 +0700

    - restore .NET workload before building

--- src/libs/DotnetBindings.Cake/Tasks/NugetTask.cs
+++ src/libs/DotnetBindings.Cake/Tasks/NugetTask.cs
@@ -1,10 +1,14 @@
-﻿namespace DotnetBindings.Cake;
+﻿using Cake.Common.Tools.DotNet.Workload.Restore;
+
+namespace DotnetBindings.Cake;
 
 [IsDependentOn(typeof(BinderateTask))]
 public sealed class NugetTask : FrostingTask<BuildContext>
 {
     public override void Run(BuildContext context)
     {
+        context.DotNetWorkloadRestore(context.SlnPath);
+
         var nugetsFolderPath = PathIO.Combine(
             context.BasePath,
             "nugets"
