58b1e3acb6fed62d42911dfaf129e52e9a8b3b43
Tuyen VU <hi@tuyen-vuduc.tech>
2025-10-25 22:30:57 +0700

    fix: org.jetbrains.kotlinx:kotlinx-datetime (#114)
    * feat: add kotlinx-datetime dependency and update metadata for DateTimeUnit classes
    
    * feat: update kotlinx-datetime to version 0.4.1 and add JSON metadata files
    
    * feat: update kotlinx-datetime to version 0.5.0 and add JSON metadata files
    
    * feat: update kotlinx-datetime to version 0.6.0 and enhance metadata handling
    
    * feat: update kotlinx-datetime to version 0.6.1 and adjust metadata
    
    * feat: update kotlinx-datetime to version 0.6.2 and add JSON metadata files
    
    * feat: update kotlinx-datetime to version 0.7.0 and add interface implementations
    
    * feat: update kotlinx-datetime to version 0.7.1
    
    * feat: add kotlinx-datetime version 0.7.1 JSON metadata files
    
    * feat: remove nugetVersion from 0.7.1 JSON files and set dependencyOnly to true in nuget.json

--- src/android/org.jetbrains.kotlinx/kotlinx-datetime-jvm/binding/Additions/Additions.cs
+++ src/android/org.jetbrains.kotlinx/kotlinx-datetime-jvm/binding/Additions/Additions.cs
@@ -3,8 +3,83 @@ using System.Collections.Generic;
 using Android.Runtime;
 using Java.Interop;
 
-namespace Kotlinx.Datetime
+namespace KotlinX.Datetime
 {
+    partial class LocalDateRange
+    {
+        // Explicit interface implementations delegating to existing strongly-typed properties.
+        global::Java.Lang.Object global::Kotlin.Ranges.IClosedRange.EndInclusive => EndInclusive;
+        global::Java.Lang.Object global::Kotlin.Ranges.IClosedRange.Start => Start;
+        global::Java.Lang.Object global::Kotlin.Ranges.IOpenEndRange.EndExclusive => EndExclusive;
+        global::Java.Lang.Object global::Kotlin.Ranges.IOpenEndRange.Start => Start;
+    }
+    partial class LocalDateProgression
+    {
+        bool global::Java.Util.ICollection.Add(global::Java.Lang.Object obj)
+            => Add(obj as global::KotlinX.Datetime.LocalDate);
+        
+        bool Java.Util.ICollection.AddAll(System.Collections.ICollection items)
+            => AddAll(items.Cast<global::KotlinX.Datetime.LocalDate>().ToList());
+
+        bool Java.Util.ICollection.ContainsAll(System.Collections.ICollection items)
+            => ContainsAll(items.ToCollection());
+
+        bool Java.Util.ICollection.RemoveAll(System.Collections.ICollection items)
+            => RemoveAll(items.ToCollection());
+
+        bool Java.Util.ICollection.RetainAll(System.Collections.ICollection items)
+            => RetainAll(items.ToCollection());
+        int Java.Util.ICollection.Size() => Size;
+    }
+
+    partial class YearMonthRange
+    {
+        // Explicit interface implementations delegating to existing strongly-typed properties.
+        global::Java.Lang.Object global::Kotlin.Ranges.IClosedRange.EndInclusive => EndInclusive;
+        global::Java.Lang.Object global::Kotlin.Ranges.IClosedRange.Start => Start;
+        global::Java.Lang.Object global::Kotlin.Ranges.IOpenEndRange.EndExclusive => EndExclusive;
+        global::Java.Lang.Object global::Kotlin.Ranges.IOpenEndRange.Start => Start;
+    }
+    partial class YearMonthProgression
+    {
+        bool global::Java.Util.ICollection.Add(global::Java.Lang.Object obj)
+            => Add(obj as global::KotlinX.Datetime.YearMonth);
+
+        bool Java.Util.ICollection.AddAll(System.Collections.ICollection items)
+            => AddAll(items.Cast<global::KotlinX.Datetime.YearMonth>().ToList());
+
+        bool Java.Util.ICollection.ContainsAll(System.Collections.ICollection items)
+            => ContainsAll(items.ToCollection());
+
+        bool Java.Util.ICollection.RemoveAll(System.Collections.ICollection items)
+            => RemoveAll(items.ToCollection());
+
+        bool Java.Util.ICollection.RetainAll(System.Collections.ICollection items)
+            => RetainAll(items.ToCollection());
+        int Java.Util.ICollection.Size() => Size;
+    }
+    partial class DateTimeUnit
+    {
+        partial class DayBased
+        {
+            public override global::KotlinX.Datetime.DateTimeUnit Times(int scalar) => Times_(scalar);
+        }
+        partial class MonthBased
+        {
+            public override global::KotlinX.Datetime.DateTimeUnit Times(int scalar) => Times_(scalar);
+        }
+        partial class TimeBased
+        {
+            public override global::KotlinX.Datetime.DateTimeUnit Times(int scalar) => Times_(scalar);
+        }
+    }
+
+    partial class YearMonth : global::Java.Lang.IComparable
+    {
+        int Java.Lang.IComparable.CompareTo(Java.Lang.Object o)
+            => CompareTo(o as YearMonth);
+    }
+
     partial class LocalTime : global::Java.Lang.IComparable
     {
         int Java.Lang.IComparable.CompareTo(Java.Lang.Object o)
@@ -20,113 +95,12 @@ namespace Kotlinx.Datetime
         int Java.Lang.IComparable.CompareTo(Java.Lang.Object o)
             => CompareTo(o as LocalDate);
     }
-    partial class Instant : global::Java.Lang.IComparable
-    {
-        int Java.Lang.IComparable.CompareTo(Java.Lang.Object o)
-            => CompareTo(o as Instant);
-    }
 }
-namespace Kotlinx.Datetime.Internal
+
+static class X
 {
-    partial class DecimalFraction : global::Java.Lang.IComparable
+    public static System.Collections.Generic.ICollection<object> ToCollection(this System.Collections.ICollection collection)
     {
-        int Java.Lang.IComparable.CompareTo(Java.Lang.Object o)
-            => CompareTo(o as DecimalFraction);
-    }
-}
-namespace Kotlinx.Datetime.Format
-{
-    partial class DateTimeComponentsFormat
-    {
-        protected override global::Kotlinx.Datetime.Internal.Format.CachedFormatStructure RawActualFormat => ActualFormat;
-
-        protected override global::Java.Lang.Object RawEmptyIntermediate => EmptyIntermediate;
-
-        public override global::Java.Lang.Object IntermediateFromValue(global::Java.Lang.Object? p0)
-            => IntermediateFromValueX(p0 as global::Kotlinx.Datetime.Format.DateTimeComponents);
-
-        public override global::Java.Lang.Object? ValueFromIntermediate(global::Java.Lang.Object p0)
-            => ValueFromIntermediateX(p0 as global::Kotlinx.Datetime.Format.DateTimeComponentsContents);
-    }
-    partial class UtcOffsetFormat
-    {
-        protected override global::Kotlinx.Datetime.Internal.Format.CachedFormatStructure RawActualFormat => ActualFormat;
-
-        protected override global::Java.Lang.Object RawEmptyIntermediate => EmptyIntermediate;
-
-        public override global::Java.Lang.Object IntermediateFromValue(global::Java.Lang.Object? p0)
-            => IntermediateFromValueX(p0 as global::Kotlinx.Datetime.UtcOffset);
-
-        public override global::Java.Lang.Object? ValueFromIntermediate(global::Java.Lang.Object p0)
-            => ValueFromIntermediateX(p0 as global::Kotlinx.Datetime.Format.IncompleteUtcOffset);
-    }
-    partial class LocalTimeFormat
-    {
-        protected override global::Kotlinx.Datetime.Internal.Format.CachedFormatStructure RawActualFormat => ActualFormat;
-
-        protected override global::Java.Lang.Object RawEmptyIntermediate => EmptyIntermediate;
-
-        public override global::Java.Lang.Object IntermediateFromValue(global::Java.Lang.Object? p0)
-            => IntermediateFromValueX(p0 as global::Kotlinx.Datetime.LocalTime);
-
-        public override global::Java.Lang.Object? ValueFromIntermediate(global::Java.Lang.Object p0)
-            => ValueFromIntermediateX(p0 as global::Kotlinx.Datetime.Format.IncompleteLocalTime);
-    }
-    partial class LocalDateFormat
-    {
-        protected override global::Kotlinx.Datetime.Internal.Format.CachedFormatStructure RawActualFormat => ActualFormat;
-
-        protected override global::Java.Lang.Object RawEmptyIntermediate => EmptyIntermediate;
-
-        public override global::Java.Lang.Object IntermediateFromValue(global::Java.Lang.Object? p0)
-            => IntermediateFromValueX(p0 as global::Kotlinx.Datetime.LocalDate);
-
-        public override global::Java.Lang.Object? ValueFromIntermediate(global::Java.Lang.Object p0)
-            => ValueFromIntermediateX(p0 as global::Kotlinx.Datetime.Format.IncompleteLocalDate);
-    }
-    partial class LocalDateTimeFormat
-    {
-        protected override global::Kotlinx.Datetime.Internal.Format.CachedFormatStructure RawActualFormat => ActualFormat;
-
-        protected override global::Java.Lang.Object RawEmptyIntermediate => EmptyIntermediate;
-
-        public override global::Java.Lang.Object IntermediateFromValue(global::Java.Lang.Object? p0)
-            => IntermediateFromValueX(p0 as global::Kotlinx.Datetime.LocalDateTime);
-
-        public override global::Java.Lang.Object? ValueFromIntermediate(global::Java.Lang.Object p0)
-            => ValueFromIntermediateX(p0 as global::Kotlinx.Datetime.Format.IncompleteLocalDateTime);
-    }
-}
-namespace Kotlinx.Datetime.Internal.Format
-{
-    partial class AbstractFieldSpec : IFieldSpec
-    {
-        IAccessor IFieldSpec.Accessor => Accessor;
-
-        Java.Lang.Object? IFieldSpec.DefaultValue => DefaultValue;
-
-        IFieldSign? IFieldSpec.Sign => Sign;
-    }
-    partial class UnsignedFieldSpec {
-        protected override unsafe global::Java.Lang.Integer? DefaultValue {
-			// Metadata.xml XPath method reference: path="/api/package[@name='kotlinx.datetime.internal.format']/class[@name='UnsignedFieldSpec']/method[@name='getDefaultValue' and count(parameter)=0]"
-			[Register ("getDefaultValue", "()Ljava/lang/Integer;", "")]
-			get {
-				const string __id = "getDefaultValue.()Ljava/lang/Integer;";
-				try {
-					var __rm = _members.InstanceMethods.InvokeAbstractObjectMethod (__id, this, null);
-					return global::Java.Lang.Object.GetObject<global::Java.Lang.Integer> (__rm.Handle, JniHandleOwnership.TransferLocalRef);
-				} finally {
-				}
-			}
-		}
-    }
-    partial class UnsignedIntFieldFormatDirective : global::Kotlinx.Datetime.Internal.Format.IFieldFormatDirective
-    {
-        IFieldSpec IFieldFormatDirective.Field => Field;
-    }
-    partial class NamedUnsignedIntFieldFormatDirective : global::Kotlinx.Datetime.Internal.Format.IFieldFormatDirective
-    {
-        IFieldSpec IFieldFormatDirective.Field => Field;
+        return collection.Cast<object>().ToList();
     }
 }
