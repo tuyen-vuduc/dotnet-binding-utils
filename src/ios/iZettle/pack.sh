ac32a6fe619f61b3dd7145882e786ea30d531482
Tuyen Vu <hi@tuyen-vuduc.tech>
2025-10-25 07:48:25 +0700

    feat: restore iZettleSDK.iOS.targets and update pack.sh for correct project path
    (cherry picked from commit 03bbd1ca5d345d4684ccddf993d090df74203372)

--- src/ios/iZettle/pack.sh
+++ src/ios/iZettle/pack.sh
@@ -1,2 +1,2 @@
 dotnet nuget locals -c all
-dotnet pack -c Release -t:Clean,Rebuild iZettle.iOS/iZettle.iOS.csproj --output ../../../nugets
+dotnet pack -c Release -t:Clean,Rebuild iZettleSDK.iOS/iZettleSDK.iOS.csproj --output ../../../nugets
