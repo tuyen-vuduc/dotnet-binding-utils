5b84e8985a3c60a5edd824d3393c2c804b1e22c8
Tuyen Vu <hi@tuyen-vuduc.tech>
2025-10-25 21:52:14 +0700

    feat: update kotlinx-datetime to version 0.6.2 and add JSON metadata files

--- src/android/org.jetbrains.kotlinx/kotlinx-datetime-jvm/binding/Additions/Additions.cs
+++ src/android/org.jetbrains.kotlinx/kotlinx-datetime-jvm/binding/Additions/Additions.cs
@@ -42,37 +42,3 @@ namespace KotlinX.Datetime
             => CompareTo(o as Instant);
     }
 }
-
-namespace KotlinX.Datetime.Format
-{
-    partial class DateTimeComponentsFormat
-    {
-        public override Java.Lang.Object GetEmptyIntermediate() => GetEmptyIntermediate_();
-        public override Java.Lang.Object IntermediateFromValue(Java.Lang.Object? p0) => IntermediateFromValue(p0 as global::KotlinX.Datetime.Format.DateTimeComponents);
-        override public Java.Lang.Object ValueFromIntermediate(Java.Lang.Object p0) => ValueFromIntermediate(p0 as global::KotlinX.Datetime.Format.DateTimeComponentsContents);
-    }
-    partial class LocalDateFormat
-    {
-        public override Java.Lang.Object GetEmptyIntermediate() => GetEmptyIntermediate_();
-        public override Java.Lang.Object IntermediateFromValue(Java.Lang.Object? p0) => IntermediateFromValue(p0 as global::KotlinX.Datetime.LocalDate);
-        override public Java.Lang.Object ValueFromIntermediate(Java.Lang.Object p0) => ValueFromIntermediate(p0 as global::KotlinX.Datetime.Format.IncompleteLocalDate);
-    }
-    partial class LocalTimeFormat
-    {
-        public override Java.Lang.Object GetEmptyIntermediate() => GetEmptyIntermediate_();
-        public override Java.Lang.Object IntermediateFromValue(Java.Lang.Object? p0) => IntermediateFromValue(p0 as global::KotlinX.Datetime.LocalTime);
-        override public Java.Lang.Object ValueFromIntermediate(Java.Lang.Object p0) => ValueFromIntermediate(p0 as global::KotlinX.Datetime.Format.IncompleteLocalTime);
-    }
-    partial class LocalDateTimeFormat
-    {
-        public override Java.Lang.Object GetEmptyIntermediate() => GetEmptyIntermediate_();
-        public override Java.Lang.Object IntermediateFromValue(Java.Lang.Object? p0) => IntermediateFromValue(p0 as global::KotlinX.Datetime.LocalDateTime);
-        override public Java.Lang.Object ValueFromIntermediate(Java.Lang.Object p0) => ValueFromIntermediate(p0 as global::KotlinX.Datetime.Format.IncompleteLocalDateTime);
-    }
-    partial class UtcOffsetFormat
-    {
-        public override Java.Lang.Object GetEmptyIntermediate() => GetEmptyIntermediate_();
-        public override Java.Lang.Object IntermediateFromValue(Java.Lang.Object? p0) => IntermediateFromValue(p0 as global::KotlinX.Datetime.UtcOffset);
-        override public Java.Lang.Object ValueFromIntermediate(Java.Lang.Object p0) => ValueFromIntermediate(p0 as global::KotlinX.Datetime.Format.IncompleteUtcOffset);
-    }
-}
