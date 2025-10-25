0491989309bc80cafb52b03e95544286068c9877
Tuyen Vu <hi@tuyen-vuduc.tech>
2025-10-25 15:53:52 +0700

    Remove unused files and XML mappings related to Additions and Transforms in the Zettle SDK feature for PayPal UI. This includes the deletion of various AboutAdditions.txt, Additions.cs, EnumFields.xml, EnumMethods.xml, and Metadata.xml files across multiple directories, streamlining the codebase by eliminating obsolete components.

--- qs/com.izettle.payments--android-sdk-ui/source/com.izettle.android.qrc/venmo-qrc-sdk/Additions/Additions.cs
+++
@@ -1,8 +0,0 @@
-namespace Com.Izettle.Android.Qrc.Venmo.UI.Payment
-{
-    partial class VenmoQrcPaymentActivity
-    {
-        public override global::AndroidX.Fragment.App.Fragment OnCreatePaymentFragment()
-            => OnCreatePaymentFragmentX();
-    }
-}
